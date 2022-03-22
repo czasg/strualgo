b21fdf6d844650ac48556259d24a63988e2367ee
czasg <czasg0.0>
2022-03-22 12:49:37 +0800

    feat: 快速排序

--- algo/sort/insertion.go
+++ algo/sort/insertion.go
@@ -1,45 +1,47 @@
 package sort
 
-func InsertionSortASC(nums []int) []int {
-    newNums := make([]int, len(nums))
-    for index, num := range nums {
-        newNums[index] = num
-    }
-    if len(newNums) < 2 {
-        return newNums
-    }
-    for i := 1; i < len(newNums); i++ {
-        index := i
-        for index > 0 {
-            if newNums[index-1] > newNums[index] {
-                newNums[index-1], newNums[index] = newNums[index], newNums[index-1]
-            } else {
-                break
-            }
-            index--
-        }
-    }
-    return newNums
+import "github.com/czasg/strualgo/basis"
+
+func InsertionSortASC[Number basis.Number](nums []Number) []Number {
+	newNums := make([]Number, len(nums))
+	for index, num := range nums {
+		newNums[index] = num
+	}
+	if len(newNums) < 2 {
+		return newNums
+	}
+	for i := 1; i < len(newNums); i++ {
+		index := i
+		for index > 0 {
+			if newNums[index-1] > newNums[index] {
+				newNums[index-1], newNums[index] = newNums[index], newNums[index-1]
+			} else {
+				break
+			}
+			index--
+		}
+	}
+	return newNums
 }
 
-func InsertionSortDESC(nums []int) []int {
-    newNums := make([]int, len(nums))
-    for index, num := range nums {
-        newNums[index] = num
-    }
-    if len(newNums) < 2 {
-        return newNums
-    }
-    for i := 1; i < len(newNums); i++ {
-        index := i
-        for index > 0 {
-            if newNums[index-1] < newNums[index] {
-                newNums[index-1], newNums[index] = newNums[index], newNums[index-1]
-            } else {
-                break
-            }
-            index--
-        }
-    }
-    return newNums
+func InsertionSortDESC[Number basis.Number](nums []Number) []Number {
+	newNums := make([]Number, len(nums))
+	for index, num := range nums {
+		newNums[index] = num
+	}
+	if len(newNums) < 2 {
+		return newNums
+	}
+	for i := 1; i < len(newNums); i++ {
+		index := i
+		for index > 0 {
+			if newNums[index-1] < newNums[index] {
+				newNums[index-1], newNums[index] = newNums[index], newNums[index-1]
+			} else {
+				break
+			}
+			index--
+		}
+	}
+	return newNums
 }
